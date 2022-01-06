d020670f99550628944ee7e094c954cd2df8c865
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-06 18:17:33 +0300

    Model/General
    Dönülecek veriler için ortak bir yapı oluşturuldu.

---
+++ Kemiksiz/Kemiksiz.Model/General.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Model
+{
+    public class General<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Entity { get; set; }
+        public List<T> List { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string SuccessfulMessage { get; set; }
+    }
+}

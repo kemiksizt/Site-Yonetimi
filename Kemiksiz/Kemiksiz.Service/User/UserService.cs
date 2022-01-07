90cdaab5bc189843d58180639fee984ef2c64fde
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-07 22:09:21 +0300

    create Service UserService
    Servis katmanında User için yapı oluşturuldu

---
+++ Kemiksiz/Kemiksiz.Service/User/UserService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Service.User
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper mapper;
+
+        public UserService(IMapper _mapper)
+        {
+            mapper = _mapper;
+        }
+    }
+}

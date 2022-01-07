e28c9b5389d01c21636367a6e29d8f432b175baa
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-07 22:13:59 +0300

    create APIcontroller usercontroller
    API katmanında UserController oluşturuldu.

---
+++ Kemiksiz/Kemiksiz.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Kemiksiz.Service.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kemiksiz.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            userService = _userService;
+        }
+    }
+}

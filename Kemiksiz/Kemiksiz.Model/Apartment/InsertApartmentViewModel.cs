01e526cc8abbd2a1d43fd55344464117341eb2c3
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-07 20:25:41 +0300

    feat apartment model
    InsertApartmentViewModel created

---
+++ Kemiksiz/Kemiksiz.Model/Apartment/InsertApartmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Model.Apartment
+{
+    public class InsertApartmentViewModel
+    {
+        public string BlockName { get; set; }
+        public string ApartmentType { get; set; }
+        public int ApartmentNo { get; set; }
+        public int ApartmentFloor { get; set; }
+    }
+}

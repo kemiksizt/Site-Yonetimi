bc1c52dc1142eef74f84c8459c61f6c412ebfe0f
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-07 23:45:39 +0300

    create Model InsertUserViewModel
    Insert işlemi için model oluşturuldu

---
+++ Kemiksiz/Kemiksiz.Model/User/InsertUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Model.User
+{
+    public class InsertUserViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}

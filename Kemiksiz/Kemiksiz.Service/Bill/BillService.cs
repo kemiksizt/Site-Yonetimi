7203f666823e19be7bd1575a873950e5f0d0eb92
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-10 12:59:17 +0300

    feat BillService GetBills
    Faturaları getiren metod yazıldı.

--- Kemiksiz/Kemiksiz.Service/Bill/BillService.cs
+++ Kemiksiz/Kemiksiz.Service/Bill/BillService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Kemiksiz.DB.Entities.DataContext;
+using Kemiksiz.Model;
+using Kemiksiz.Model.Bill;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,38 @@ namespace Kemiksiz.Service.Bill
 {
     public class BillService : IBillService
     {
+        private readonly IMapper mapper;
+
+        public BillService(IMapper _mapper)
+        {
+            mapper = _mapper;
+        }
+
+        public General<BillViewModel> GetBills()
+        {
+            var result = new General<BillViewModel>();
+
+            using (var context = new KemiksizContext())
+            {
+                var data = context.Bill.OrderBy(a => a.Id).ThenBy(a => a.UserId)
+                                        .ThenBy(a => a.ApartmentId).ThenBy(a => a.BillType);
+
+
+                if (data.Any())
+                {
+                    result.List = mapper.Map<List<BillViewModel>>(data);
+                    result.IsSuccess = true;
+                    result.Message = "Fatura listeleme işlemi başarılı!";
+                }
+
+                else
+                {
+                    result.ExceptionMessage = "Sistemde hiçbir fatura yok!";
+                }
+
+            }
+
+            return result;
+        }
     }
 }
--- Kemiksiz/Kemiksiz.Service/Bill/IBillService.cs
+++ Kemiksiz/Kemiksiz.Service/Bill/IBillService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kemiksiz.Model;
+using Kemiksiz.Model.Bill;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace Kemiksiz.Service.Bill
 {
     public interface IBillService
     {
+        public General<BillViewModel> GetBills();
     }
 }

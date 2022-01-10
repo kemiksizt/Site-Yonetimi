70b64bddf761576579bfa0cb516d2f88052aa8c2
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-10 23:58:57 +0300

    feat RandomPassword UserService
    Register işleminde parolayı manuel olarak girmiyoruz. Kayıt sonrası otomatik 8 karakterli bir şifre kendi oluşup DB ye yazılıyor.

--- Kemiksiz/Kemiksiz.Model/User/InsertUserViewModel.cs
+++ Kemiksiz/Kemiksiz.Model/User/InsertUserViewModel.cs
@@ -11,7 +11,6 @@ namespace Kemiksiz.Model.User
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public bool IsAdmin { get; set; }
         public string Tc { get; set; }
         public string Phone { get; set; }
--- Kemiksiz/Kemiksiz.Service/User/UserService.cs
+++ Kemiksiz/Kemiksiz.Service/User/UserService.cs
@@ -19,6 +19,18 @@ namespace Kemiksiz.Service.User
             mapper = _mapper;
         }
 
+        string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
         public General<UserViewModel> GetUsers()
         {
             var result = new General<UserViewModel>();
@@ -125,6 +137,7 @@ namespace Kemiksiz.Service.User
 
                     else
                     {
+                        data.Password = CreatePassword(8);
                         data.Idate = DateTime.Now;
                         context.User.Add(data);
                         context.SaveChanges();

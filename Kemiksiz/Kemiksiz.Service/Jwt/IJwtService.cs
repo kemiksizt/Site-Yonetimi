0896e8aeaf7903fe9d6ddf6183dfc8331150b5c8
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-11 22:15:41 +0300

    feat ServiceLayer JwtService
    token kullanımı için JwtService oluşturuldu. İçerisine Generate metodu yazıldı.

---
+++ Kemiksiz/Kemiksiz.Service/Jwt/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Service.Jwt
+{
+    public interface IJwtService
+    {
+        public string Generate(int id);
+    }
+}
---
+++ Kemiksiz/Kemiksiz.Service/Jwt/JwtService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemiksiz.Service.Jwt
+{
+    public class JwtService : IJwtService
+    {
+        private string secureKey = "this is a very secure key";
+
+        public string Generate(int id)
+        {
+            var symmetricSecureKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symmetricSecureKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1)); // 1 gün
+            var securityToken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+    }
+}

8b6fff62ed93fa40063bd833b5266915ba2d5e5a
Turab Kemiksiz <turabkemiksiz@gmail.com>
2022-01-12 14:20:43 +0300

    Update LoginViewModel
    LoginViewModel güncellendi

--- Kemiksiz/Kemiksiz.Model/User/LoginViewModel.cs
+++ Kemiksiz/Kemiksiz.Model/User/LoginViewModel.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Kemiksiz.Model.User
 {
     public class LoginViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; }
+        [JsonIgnore] public string Password { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
